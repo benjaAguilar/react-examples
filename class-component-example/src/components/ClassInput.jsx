55a4be9b84e688bf7d8d77305f4dc400205e992a
Benja Aguilar <benjaaguilar02@gmail.com>
2024-06-10 11:01:06 -0300

    Solved class-component-example

--- class-component-example/src/components/ClassInput.jsx
+++ class-component-example/src/components/ClassInput.jsx
@@ -8,10 +8,15 @@ class ClassInput extends Component {
     this.state = {
       todos: ['Just some demo tasks', 'As an example'],
       inputVal: '',
+      toEdit: '',
+      editVal: '',
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.delItem = this.delItem.bind(this);
+    this.editItem = this.editItem.bind(this);
+    this.setEditInput = this.setEditInput.bind(this);
   }
 
   handleInputChange(e) {
@@ -21,11 +26,46 @@ class ClassInput extends Component {
     }));
   }
 
+  setEditInput(e) {
+    this.setState((state) => ({
+      ...state,
+      editVal: e.target.value,
+    }));
+  }
+
   handleSubmit(e) {
     e.preventDefault();
     this.setState((state) => ({
+      ...state,
       todos: state.todos.concat(state.inputVal),
       inputVal: '',
+    }));
+  }
+
+  delItem(id) {
+    const filteredTodos = this.state.todos.filter((todo) => todo !== id);
+    this.setState((state) => ({
+      ...state,
+      todos: filteredTodos,
+    }));
+  }
+
+  editItem(todo) {
+    this.setState((state) => ({
+      ...state,
+      toEdit: todo,
+    }));
+  }
+
+  doneEdit(toEditTodo) {
+    const index = this.state.todos.indexOf(toEditTodo);
+    this.state.todos[index] = this.state.editVal;
+
+    this.setState((state) => ({
+      ...state,
+      todos: this.state.todos,
+      toEdit: '',
+      editVal: '',
     }));
   }
 
@@ -50,9 +90,33 @@ class ClassInput extends Component {
         {/* The list of all the To-Do's, displayed */}
         <ul>
           {this.state.todos.map((todo) => (
-            <li key={todo}>{todo}</li>
+            <div>
+              {this.state.toEdit === todo ? (
+                <div key={todo}>
+                  <input
+                    type="text"
+                    value={this.state.editVal}
+                    onChange={(e) => this.setEditInput(e)}
+                  />
+                  <button type="button" onClick={() => this.doneEdit(todo)}>
+                    Done
+                  </button>
+                </div>
+              ) : (
+                <div key={todo}>
+                  <li key={todo}>{todo}</li>
+                  <button type="button" onClick={() => this.delItem(todo)}>
+                    del
+                  </button>
+                  <button type="button" onClick={() => this.editItem(todo)}>
+                    edit
+                  </button>
+                </div>
+              )}
+            </div>
           ))}
         </ul>
+        <p>Number of todos: {this.state.todos.length}</p>
       </section>
     );
   }
